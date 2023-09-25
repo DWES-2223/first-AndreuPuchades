c16e5ad6d579736a5e0b0ed3b9cc58a0f99566e9
Andreu Puchades <andpucpas@alu.edu.gva.es>
2023-09-25 18:28:32 +0200

    Tercero Push

--- php/src/231.php
+++ php/src/231.php
@@ -10,15 +10,36 @@ extract($_POST);
 
 <body>
 <?php
-echo '<ul>';
+//Calcular Array:
 $array = [];
 for ($i = 0; $i < 33; $i++) {
     $array[$i] = rand(0, 100);
 }
+
+//Calcular Mitjana:
+$num = 0;
 for ($i = 0; $i < count($array); $i++) {
-    echo '<li>'. $array[$i]. '<li/>';
+    $num += $array[$i];
 }
-echo '<ul>';
+echo '<p>Mitjana: '. ($num / count($array)). '<p/><br>';
+
+//Calcular Numero Major:
+$num = 0;
+for ($i = 0; $i < count($array); $i++) {
+    if($num < $array[$i]){
+        $num = $array[$i];
+    }
+}
+echo '<p>Major: '. $num. '<p/><br>';
+
+//Calcular Numero Menor:
+$num = 0;
+for ($i = 0; $i < count($array); $i++) {
+    if($num > $array[$i]){
+        $num = $array[$i];
+    }
+}
+echo '<p>Menor: '. ($num / count($array)). '<p/><br>';
 ?>
 </body>
 </html>
